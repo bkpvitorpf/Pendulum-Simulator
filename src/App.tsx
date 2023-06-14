a6ddb65fd38a2bdbc84a21f1da86776a8f11e12f
Vitor Pereira Fontes <vitor@pop-os.localdomain>
2023-06-13 21:41:29 -0300

    Configurando o layout da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="w-full h-full flex-col">
+        <div className="flex align-middle justify-center bg-red-400 items-center" style={{ height: '5%' }}>
+          <h1 className="">Pendulum SImulator</h1>
+        </div>
+        <div className="flex" style={{ height: '95%' }}>
+          <main className="w-4/5 h-full bg-gray-600 box-border p-2">
+            Simulador
+          </main>
+          <aside className="w-1/5 h-full bg-red-200 p-2">
+            Parâmetros
+          </aside>
+        </div>
+      </div>
     </>
   )
 }
